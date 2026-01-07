685279c4116b0028388715bbe1c6b7b998ffaa00
Hanner123 <hannakattermann@gmail.com>
2026-01-07 15:56:34 +0100

    dvc ignore venv

--- measure/export_vision.py
+++ measure/export_vision.py
@@ -38,26 +38,13 @@ import yaml
 RADIOML_PATH = R"/home/hanna/git/radioml-transformer/data/GOLD_XYZ_OSC.0001_1024.hdf5"
 RADIOML_PATH_NPZ = R"/home/hanna/git/radioml-transformer/data/GOLD_XYZ_OSC.0001_1024.npz"
 
-
-
-
-
-
-
-
-
-
 # Path to the CIFAR-10 dataset
 CIFAR10_ROOT = os.environ.setdefault("CIFAR10_ROOT", "data")
-
-# Export function mapping
-EXPORTERS = {"qonnx": export_qonnx, "qcdq": export_onnx_qcdq}
 
 
 # Exports the model to ONNX in conjunction with an input-output pair for
 # verification
-def export(model, dataset, batch_size, format="qonnx", split_heads=False,
-           **kwargs):
+def export(model, model_int8, dataset, batch_size, split_heads=False, **kwargs):  # noqa
     # Do the forward pass for generating verification data and tracing the model
     # for export on CPU only
     device = "cpu"
@@ -100,67 +87,6 @@ def export(model, dataset, batch_size, format="qonnx", split_heads=False,
     with torch.no_grad():
         out = model(inp)
 
-    # Export the model to ONNX using the input example
-    EXPORTERS[format](model, (inp,), "outputs/vision/model.onnx", **kwargs)
-
-    # Save the input and output data for verification purposes later
-    np.save("outputs/vision/inp.npy", inp.numpy())
-    np.save("outputs/vision/out.npy", out.numpy())
-    np.save("outputs/vision/cls.npy", cls.numpy())
-
-
-# Script entrypoint
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-# Exports the model to ONNX in conjunction with an input-output pair for
-# verification
-def export(model, model_int8, dataset, batch_size, split_heads=False, **kwargs):  # noqa
-    # Do the forward pass for generating verification data and tracing the model
-    # for export on CPU only
-    device = "cpu"
-    # Move the model to the training device
-    model = model.to(device)  # noqa: Shadows model...
-    # model_int8 = model_int8.to(device)  # noqa: Shadows model...
-    # Set model to evaluation mode
-    model = model.eval()  # noqa: Shadows model...
-    # model_int8 = model_int8.eval()  # noqa: Shadows model...
-
-    # Explicitly splits all attention heads in the model graph to be parallel
-    if split_heads:
-        # Iterate all modules in the model container and check for instances of
-        # quantized multihead attention
-        for name, module in model.named_modules():
-            if isinstance(module, QuantMultiheadAttention):
-                # Marks to take the split path next forward call
-                module.split_heads = True
-        # for name, module in model_int8.named_modules():
-        #     if isinstance(module, QuantMultiheadAttention):
-        #         # Marks to take the split path next forward call
-        #         module.split_heads = True
-
-    # Load the RadioML dataset splits as configured
-    _, _, eval_data = get_datasets(path=RADIOML_PATH, **dataset)
-    # Create a batched and shuffled data loader the ImageNet validation split
-    export_data = DataLoader(eval_data, batch_size=batch_size, shuffle=True)
-
-    # Sample the first batch from the export dataset
-    inp, out, _ = next(iter(export_data))
 
     # Export the model to ONNX using the input example
     export_qonnx(model, (inp,), "outputs/vision/model.onnx", **kwargs)
@@ -168,6 +94,8 @@ def export(model, model_int8, dataset, batch_size, split_heads=False, **kwargs):
     # Save the input and output data for verification purposes later
     np.save("outputs/vision/inp.npy", inp.numpy())
     np.save("outputs/vision/out.npy", out.numpy())
+    np.save("outputs/vision/cls.npy", cls.numpy())
+
     # Standard ONNX export for reference - works with dynamic batch sizes
     onnx_path = "outputs/vision/model_dynamic_batchsize.onnx"
     torch.onnx.export(
