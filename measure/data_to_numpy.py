9563f7d5e4fb1764bf7a39d0c0328a8d523e907e
Hanner123 <hannakattermann@gmail.com>
2026-01-22 15:25:57 +0100

    transform in numpy file

--- measure/data_to_numpy.py
+++ measure/data_to_numpy.py
@@ -1,10 +1,23 @@
 import os
 import pickle
 import numpy as np
+from torchvision import transforms
+import torch
 
 CIFAR10_ROOT = R"/data/gitlab/cifar-10-batches-py"
 OUT_FILE = os.path.join(CIFAR10_ROOT, "cifar10.npz")
 
+# Transformation to be applied to the input images: Rather basic
+# preprocessing turning images into tensors and normalizing with only
+# minimal data augmentation
+tf = transforms.Compose([
+    # Convert from PIL image to PyTorch tensors
+    transforms.ToTensor(),
+    # Random horizontal flip in 50% of the cases
+    transforms.RandomHorizontalFlip(),
+    # CIFAR-10 statistics on the whole training set
+    transforms.Normalize([0.4914, 0.4822, 0.4465], [0.2470, 0.2435, 0.2616])
+])
 
 
 
@@ -33,9 +46,19 @@ labels_all.append(lbls)
 images_all = np.concatenate(images_all)
 labels_all = np.concatenate(labels_all)
 
+images_all_transformed = []
+
+for i in range(images_all.shape[0]):
+    img = images_all[i].transpose(1, 2, 0)  # CIFAR Pickle ist (C,H,W)
+    img = tf(img)  # -> tensor float32 [0,1] + normalize
+    images_all_transformed.append(img.numpy())
+
+images_all_transformed = np.stack(images_all_transformed)
+
+
 np.savez_compressed(
     OUT_FILE,
-    images=images_all,
+    images=images_all_transformed,
     labels=labels_all
 )
 
