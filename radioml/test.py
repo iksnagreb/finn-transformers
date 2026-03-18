0b47febbb86a9b6229bc6f2876ef5dc5728690a7
Hanner123 <hannakattermann@gmail.com>
2026-03-18 13:30:21 +0100

    deleted measure files in radioml folder

--- radioml/test.py
+++
@@ -1,6 +0,0 @@
-import onnx
-
-model = onnx.load("/home/hanna/git/finn-transformers/outputs/radioml/model_brevitas_1.onnx")
-nodes = [node.op_type for node in model.graph.node]
-from collections import Counter
-print(Counter(nodes))
